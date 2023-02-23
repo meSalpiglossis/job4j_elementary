46e4d8d4efea1b0f567aefa48786cee167fef111
meSalpiglossis <125427233+meSalpiglossis@users.noreply.github.com>
2023-02-23 22:28:12 +1000

    4.3. Вызов метода без указания класса [#504945]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,7 +1,13 @@
 package ru.job4j;
 
+import ru.job4j.calculator.Calculator;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world");
+        int age = 18;
+        float length = 78.0332F;
+        boolean baby = true;
+
     }
 }
