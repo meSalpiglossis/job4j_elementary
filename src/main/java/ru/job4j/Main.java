7f98519a7afb31daac80f233fc0b2561a4a3ce5b
meSalpiglossis <125427233+meSalpiglossis@users.noreply.github.com>
2023-02-25 18:14:54 +1000

    replace test

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -4,10 +4,11 @@ import ru.job4j.calculator.Calculator;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
-        int age = 18;
-        float length = 78.0332F;
-        boolean baby = true;
+        var emoji = "-(";
+        // BEGIN (write your solution here)
+        String emoji2 = ":" + emoji.replace("(", ")");
+        System.out.println(emoji2);
+        // END
 
     }
 }
