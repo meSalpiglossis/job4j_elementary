bdbc4b7625b414b9f87ad1895c6498f66031fdd3
meSalpiglossis <125427233+meSalpiglossis@users.noreply.github.com>
2023-02-25 19:31:22 +1000

    main checkstyle

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -5,10 +5,7 @@ import ru.job4j.calculator.Calculator;
 public class Main {
     public static void main(String[] args) {
         var emoji = "-(";
-
         String emoji2 = ":" + emoji.replace("(", ")");
         System.out.println(emoji2);
-
-
     }
 }
