b09d07fab7d70794dc5335209fcd550a485b2078
meSalpiglossis <125427233+meSalpiglossis@users.noreply.github.com>
2023-02-25 18:52:50 +1000

    remove comments

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -5,10 +5,10 @@ import ru.job4j.calculator.Calculator;
 public class Main {
     public static void main(String[] args) {
         var emoji = "-(";
-        // BEGIN (write your solution here)
+
         String emoji2 = ":" + emoji.replace("(", ")");
         System.out.println(emoji2);
-        // END
+
 
     }
 }
